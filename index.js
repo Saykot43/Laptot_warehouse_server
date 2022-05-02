e0db6cdb4e73dcf5cd3f2c63d9b17e69f401c296
Saykot43 <saykot.cse43@gmail.com>
2022-05-02 12:57:28 +0600

    read api create

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
 const port = process.env.port || 5000
 
+app.use(cors())
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_PASS}@ass11.w5qxo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    
+    await client.connect();
+
+    const productCollection = client.db("products").collection("product");
+
+    // Read all data
+
+    app.get('/produts', async(req, res)=>{
+        const quary = req.query;
+        const cursor = productCollection.find({});
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    // Create a data
+
+    // Update a data
+
+    // Delete a data
+
+
+    // const result = await productCollection.insertOne();
+    console.log("Connected successfully to server");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
