e1b2f556c23367ff052ca1d1d998aee933af3c69
Saykot43 <saykot.cse43@gmail.com>
2022-05-02 17:29:06 +0600

    create api added

--- index.js
+++ index.js
@@ -2,51 +2,76 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.port || 5000
 
+// middelware
 app.use(cors())
+app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_PASS}@ass11.w5qxo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-  try {
-    
-    await client.connect();
+    try {
 
-    const productCollection = client.db("products").collection("product");
+        await client.connect();
 
-    // Read all data
+        const productCollection = client.db("products").collection("product");
 
-    app.get('/produts', async(req, res)=>{
-        const quary = req.query;
-        const cursor = productCollection.find({});
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+        // Read all data
+        // http://localhost:5000/produts
+        app.get('/produts', async (req, res) => {
+            const quary = req.query;
+            const cursor = productCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
-    // Create a data
+        // Create a data
+        // http://localhost:5000/productAdd
+        app.post('/productAdd', async (req, res) => {
 
-    // Update a data
+            const data = req.body;
 
-    // Delete a data
+            const result = await productCollection.insertOne(data);
+            res.send(result);
+            console.log('product added successfully');
+        })
 
+        // Update a data
+        // http://localhost:5000/update/626f8730a3a7e2edde32385f
 
-    // const result = await productCollection.insertOne();
-    console.log("Connected successfully to server");
-  } finally {
-    
-  }
+        app.put('/update/:id', async (req, res)=>{
+            const id = req.params.id;
+            const data = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                ...data,
+                },
+              };
+              const result = await productCollection.updateOne(filter, updateDoc, options);
+              res.send(result);
+        })
+
+        // Delete a data
+app.
+
+        console.log("Connected successfully to server");
+    } finally {
+
+    }
 }
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
