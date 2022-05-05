cad0e9ba4deb5780709ccb45fa5323287ea7cc01
Saykot43 <saykot.cse43@gmail.com>
2022-05-06 03:40:26 +0600

    updaed

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
-// middelware
 app.use(cors())
 app.use(express.json())
 
